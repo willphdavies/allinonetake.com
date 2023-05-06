3726c730ff1221cfa3f790e5e9cd7dfe416d0d03
willphdavies <willphdavies@gmail.com>
2023-05-05 20:46:48 -0700

    STUUFF

--- src/data/albums/index.ts
+++ src/data/albums/index.ts
@@ -7,7 +7,9 @@ import a5 from './2023-03-12';
 import a6 from './2023-03-19';
 import a7 from './2023-03-26';
 import a8 from './2023-04-09';
+import a9 from './2023-04-30';
 export const albums: IAlbum[] = [
+  a9,
   a8,
   a7,
   a6,
